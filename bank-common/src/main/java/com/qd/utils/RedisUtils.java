f2827f329ed37853b66de190b151c3614c8788f1
GXW <gxw960115@outlook.com>
2022-04-16 08:53:14 +0800

    框架搭建 --PageUtil  Redis工具类

--- bank-common/src/main/java/com/qd/utils/RedisUtils.java
+++ bank-common/src/main/java/com/qd/utils/RedisUtils.java
@@ -11,9 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -78,6 +76,12 @@ public class RedisUtils {
         return redisTemplate.getExpire(key, TimeUnit.SECONDS);
     }
 
+    /**
+     * 查找匹配key
+     *
+     * @param pattern
+     * @return
+     */
     public List<String> scan(String pattern) {
         ScanOptions options = ScanOptions.scanOptions().match(pattern).build();
         RedisConnectionFactory factory = redisTemplate.getConnectionFactory();
@@ -93,5 +97,77 @@ public class RedisUtils {
             log.error(e.getMessage(), e);
         }
         return result;
+    }
+
+    /**
+     * 分页查询 key
+     *
+     * @param patternKey key
+     * @param page       页码
+     * @param size       每页数目
+     * @return
+     */
+    public List<String> findKeyForPage(String patternKey, int page, int size) {
+        ScanOptions options = ScanOptions.scanOptions().match(patternKey).build();
+        RedisConnectionFactory factory = redisTemplate.getConnectionFactory();
+        RedisConnection rc = Objects.requireNonNull(factory).getConnection();
+        Cursor<byte[]> cursor = rc.scan(options);
+        List<String> result = new ArrayList<>(size);
+        int tmpIndex = 0;
+        int fromIndex = page * size;
+        int toIndex = page * size + size;
+        while (cursor.hasNext()) {
+            if (tmpIndex >= fromIndex && tmpIndex < toIndex) {
+                result.add(new String(cursor.next()));
+                tmpIndex++;
+                continue;
+            }
+            // 获取满足条件的数据后，就可以退出了
+            if (tmpIndex >= toIndex) {
+                break;
+            }
+            tmpIndex++;
+            cursor.next();
+        }
+        try {
+            RedisConnectionUtils.releaseConnection(rc, factory);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    /**
+     * 判断key是否存在
+     *
+     * @param key 键
+     * @return true 存在 false 不存在
+     */
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return false;
+        }
+    }
+
+    public void del(String... keys) {
+        if (keys != null && keys.length > 0) {
+            Boolean result = redisTemplate.delete(keys[0]);
+            log.debug("--------------------------------------------");
+            log.debug(new StringBuilder("删除缓存：").append(keys[0]).append(",结果：").append(result).toString());
+            log.debug("--------------------------------------------");
+        } else {
+            Set<Object> keySet = new HashSet<>();
+            for (String key : keys) {
+                keySet.addAll(redisTemplate.keys(key));
+            }
+            Long count = redisTemplate.delete(keySet);
+            log.debug("--------------------------------------------");
+            log.debug("成功删除缓存：" + keySet.toString());
+            log.debug("缓存删除数量：" + count + "个");
+            log.debug("--------------------------------------------");
+        }
     }
 }
